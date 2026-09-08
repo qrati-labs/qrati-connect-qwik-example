502a0501861dd2df863b8ab4eb3653cca5f90f97
Siddhartha Maity <siddharthamaity1993@gmail.com>
2026-09-08 15:59:09 +0530

    feat: add GTM consent tracking

--- src/components/CookieConsentBanner.tsx
+++ src/components/CookieConsentBanner.tsx
@@ -1,9 +1,10 @@
 import { component$, useVisibleTask$ } from '@builder.io/qwik';
-import { checkConsentRequired, ensureCookieConsentInitialized } from '../lib/cookieConsent';
+import { checkConsentRequired, ensureCookieConsentInitialized, initGtm } from '../lib/cookieConsent';
 
 export const CookieConsentBanner = component$(() => {
   // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(async () => {
+    initGtm();
     try {
       const required = await checkConsentRequired();
       if (required) {
